3c3711e1dd393b80bdeb07db96a101e61a2b258f
unknown <sonwj0915@naver.com>
2021-05-01 15:36:11 +0900

    표지 타이틀 설정

--- src/page/pageSet/Homepage/Home.js
+++ src/page/pageSet/Homepage/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./css/Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="container" id="container">
+        <h1 className="title" id="title">
+          <span id="title1">WELCOME</span> &nbsp;
+          <span id="title2">MY</span> &nbsp;
+          <span id="title3">PORTFOLIO</span>
+        </h1>
+      </div>
+    </>
+  );
 }
 
 export default Home;
--- src/page/pages/Homepage.js
+++ src/page/pages/Homepage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Back3d from "../pageSet/Homepage/Back3d";
+import Home from "../pageSet/Homepage/Home";
 
 function Homepage() {
   return (
     <div>
+      <Home />
       <Back3d />
     </div>
   );
