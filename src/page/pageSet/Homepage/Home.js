c6ec3abd7bcbf2dcb045ae8da5b432e32f41ef32
unknown <sonwj0915@naver.com>
2021-05-01 15:45:59 +0900

    link효과 추가

--- src/page/pageSet/Homepage/Home.js
+++ src/page/pageSet/Homepage/Home.js
@@ -1,9 +1,22 @@
-import React from "react";
+import gsap from "gsap/all";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Button from "./Button";
 import "./css/Home.css";
 
 function Home() {
+  useEffect(() => {
+    // link 효과
+    gsap.set("#link", {
+      opacity: 0,
+      display: "none",
+    });
+    gsap.to("#link", {
+      opacity: 1,
+      display: "block",
+      duration: 1,
+    });
+  });
   return (
     <>
       <div className="container" id="container">
