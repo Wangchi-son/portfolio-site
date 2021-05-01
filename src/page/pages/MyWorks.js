60a774b5bd183f9142995f5d2ef380e0942477e4
unknown <sonwj0915@naver.com>
2021-05-01 21:19:51 +0900

    MyWorks 페이지 틀 제작

--- src/page/pages/MyWorks.js
+++ src/page/pages/MyWorks.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React from 'react';
+import './css/MyWorks.css';
+
+import HomeButton from '../pageSet/tools/HomeButton';
 
 function MyWorks() {
-  return <div>MyWorks</div>;
+  return (
+    <>
+      <HomeButton />
+      <div className="myWorkContainer">
+        <section className="first panels blue">
+          <h1>First Page</h1>
+        </section>
+        <section className="panels red">
+          <h1>Second Page</h1>
+        </section>
+        <section className="panels orange">
+          <h1>Third Page</h1>
+        </section>
+        <section className="panels purple">
+          <h1>Fourth Page</h1>
+        </section>
+        <section className="panels green">
+          <h1>Fifth Page</h1>
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default MyWorks;
